395a638e4d3270d1300452b6edb8b60e220f683e
valchevgd <valchevgd@gmail.com>
2020-05-14 20:29:52 +0300

    Create Register page. Closes #3

---
+++ register.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Welcome To MyFlix</title>
+    <link rel="stylesheet" type="text/css" href="assets/style/style.css"/>
+</head>
+<body>
+<h1>Register!!</h1>
+</body>
+</html>
